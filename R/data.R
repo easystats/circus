9c0927944448708c41c7e1e1bae92b3d70c94ff3
Dominique Makowski <dom.mak19@gmail.com>
2019-02-17 09:54:02 +0800

    documentation

---
+++ R/data.R
@@ -0,0 +1,170 @@
+#' htest_1
+#'
+#' Model of class htest
+"htest_1"
+
+#' htest_2
+#'
+#' Model of class htest
+"htest_2"
+
+#' htest_3
+#'
+#' Model of class htest
+"htest_3"
+
+#' htest_4
+#'
+#' Model of class htest
+"htest_4"
+
+#' htest_5
+#'
+#' Model of class htest
+"htest_5"
+
+#' htest_6
+#'
+#' Model of class htest
+"htest_6"
+
+#' htest_7
+#'
+#' Model of class htest
+"htest_7"
+
+#' htest_8
+#'
+#' Model of class htest
+"htest_8"
+
+
+
+
+#' anova_1
+#'
+#' Model of class anova
+"anova_1"
+
+
+#' aov_1
+#'
+#' Model of class aov
+"aov_1"
+
+
+#' aovlist_1
+#'
+#' Model of class aovlist
+"aovlist_1"
+
+
+
+
+
+
+#' lm_1
+#'
+#' Model of class lm
+"lm_1"
+
+
+#' lm_2
+#'
+#' Model of class lm
+"lm_2"
+
+
+#' lm_3
+#'
+#' Model of class lm
+"lm_3"
+
+
+#' glm_1
+#'
+#' Model of class glm
+"glm_1"
+
+
+#' glm_2
+#'
+#' Model of class glm
+"glm_2"
+
+
+
+
+
+#' lmerMod_1
+#'
+#' Model of class lmerMod
+"lmerMod_1"
+
+
+#' merMod_1
+#'
+#' Model of class merMod
+"merMod_1"
+
+
+#' merMod_2
+#'
+#' Model of class merMod
+"merMod_2"
+
+
+
+
+
+
+
+
+
+
+#' stanreg_lm_1
+#'
+#' Model of class stanreg
+"stanreg_lm_1"
+
+
+#' stanreg_lm_2
+#'
+#' Model of class stanreg
+"stanreg_lm_2"
+
+#' stanreg_lm_3
+#'
+#' Model of class stanreg
+"stanreg_lm_3"
+
+#' stanreg_glm_1
+#'
+#' Model of class stanreg
+"stanreg_glm_1"
+
+#' stanreg_glm_2
+#'
+#' Model of class stanreg
+"stanreg_glm_2"
+
+#' stanreg_lmerMod_1
+#'
+#' Model of class stanreg
+"stanreg_lmerMod_1"
+
+#' stanreg_merMod_1
+#'
+#' Model of class stanreg
+"stanreg_merMod_1"
+
+#' stanreg_merMod_2
+#'
+#' Model of class stanreg
+"stanreg_merMod_2"
+
+
+#' stanreg_gam_1
+#'
+#' Model of class stanreg
+"stanreg_gam_1"
--- R/hello.R
+++
@@ -1,7 +0,0 @@
-#' Hello, world!
-#
-#' This is an example function named 'hello' which prints 'Hello, world!'.
-#' @keywords internal
-hello <- function() {
-  print("RrRrrRr!")
-}
