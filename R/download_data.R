6a6573a535b3e8c3f8d1ec75b83c096c025d95a4
Dominique Makowski <dom.mak19@gmail.com>
2019-02-17 11:15:27 +0800

    download_data

---
+++ R/download_data.R
@@ -0,0 +1,40 @@
+#' Download rda files from github
+#' @param url URL of the rdata file.
+#' @importFrom httr GET stop_for_status content
+#' @keywords internal
+#' @export
+download_data_github <- function(url){
+
+  new_url <- gsub("https://github", "https://raw.github", url)
+  new_url <- gsub("/blob", "", new_url)
+
+  temp_file <- tempfile()
+  on.exit(unlink(temp_file))
+
+  request <- httr::GET(new_url)
+  httr::stop_for_status(request)
+  writeBin(httr::content(request, type = "raw"), temp_file)
+
+
+  x <- load(temp_file)
+  y = get(x)
+  # Remove the old object since you've stored it in y
+  rm(x)
+  return(y)
+
+}
+
+
+#' Download circus models
+#'
+#' @param name Model name.
+#' @export
+download_model <- function(name){
+
+  url <- paste0("https://github.com/easystats/circus/blob/master/data/",
+                name,
+                ".rda")
+
+  return(download_data_github(url))
+
+}
