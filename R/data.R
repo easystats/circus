1cbc0d5224df7498204cd0a85df33ef3ebb09856
Daniel <mail@danielluedecke.de>
2019-04-25 20:20:43 +0200

    add bernoulli-model

--- R/data.R
+++ R/data.R
@@ -928,6 +928,27 @@
 #' }
 "brms_mixed_4"
 
+#' brms_mixed_5
+#'
+#' Model of class brms
+#' @examples
+#' \dontrun{
+#' library(lme4)
+#' library(brms)
+#' data(sleepstudy)
+#' set.seed(123)
+#' sleepstudy$cat <- as.factor(sample(1:5, size = 180, replace = TRUE))
+#' sleepstudy$Reaction_d <-
+#'   ifelse(sleepstudy$Reaction < median(sleepstudy$Reaction), 0, 1)
+#'
+#' brms::brm(
+#'   Reaction_d ~ Days + cat + (1 | Subject),
+#'   data = sleepstudy,
+#'   family = bernoulli()
+#' )
+#' }
+"brms_mixed_5"
+
 
 
 #' brms_mv_1
