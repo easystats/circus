310541196d8a84e74b820a0b3ea2b51f15174d32
Daniel Lüdecke <mail@danielluedecke.de>
2019-05-23 17:32:57 +0200

    add model for testing

--- R/data.R
+++ R/data.R
@@ -631,6 +631,25 @@
 
 
 
+#' glmmTMB_zi_6
+#'
+#' Model of class glmmTMB
+#' @examples
+#' \dontrun{
+#' # data prepararion: see "glmmTMB_1"
+#'
+#' glmmTMB(
+#'   count ~ child + camper + (1 + xb | persons),
+#'   ziformula = ~ child + livebait + (1 + zg + nofish | ID),
+#'   dispformula = ~xb,
+#'   data = fish,
+#'   family = truncated_poisson()
+#' )
+#' }
+"glmmTMB_zi_6"
+
+
+
 
 
 
