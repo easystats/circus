caffcd27412b8e9b9c7980ff01504211ec3235b4
Daniel Lüdecke <mail@danielluedecke.de>
2019-08-06 13:49:08 +0200

    add brms-mm-model

--- R/data.R
+++ R/data.R
@@ -1382,6 +1382,27 @@
 
 
 
+#' brms_mm_1
+#'
+#' Model of class brmsfit
+#' @examples
+#' \dontrun{
+#' set.seed(123)
+#' dat <- data.frame(
+#'   sid = c(1:300),
+#'   t1id = sample(200:215, 300, replace = TRUE),
+#'   t2id = sample(200:215, 300, replace = TRUE),
+#'   y = rnorm(300, mean = 85, sd = 8),
+#'   x = rnorm(300, mean = 50, sd = 10)
+#' )
+#'
+#' brm(y ~ x + (1 | mm(t1id, t2id)), data = dat)
+#' }
+"brms_smooth_2"
+
+
+
+
 
 
 #' betareg_1
