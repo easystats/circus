e951da3379a23e4911c27b83afbb8707d450a268
Daniel Lüdecke <mail@danielluedecke.de>
2019-08-06 14:32:38 +0200

    add brms-mm-model 2

--- R/data.R
+++ R/data.R
@@ -1398,7 +1398,25 @@
 #'
 #' brm(y ~ x + (1 | mm(t1id, t2id)), data = dat)
 #' }
-"brms_smooth_2"
+"brms_mm_1"
+
+
+#' brms_mm_2
+#'
+#' Model of class brmsfit
+#' @examples
+#' \dontrun{
+#' set.seed(123)
+#' dat <- data.frame(
+#'   y = rnorm(100), x1 = rnorm(100), x2 = rnorm(100),
+#'   g1 = sample(1:10, 100, TRUE), g2 = sample(1:10, 100, TRUE)
+#' )
+#'
+#' # multi-membership model with level specific covariate values
+#' dat$xc <- (dat$x1 + dat$x2) / 2
+#' brm(y ~ xc + (1 + mmc(x1, x2) | mm(g1, g2)), data = dat)
+#' }
+"brms_mm_2"
 
 
 
