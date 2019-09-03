7d660f82b3e055e44f1c26952dd56fd5132b0c0d
Daniel <mail@danielluedecke.de>
2019-09-03 23:20:40 +0200

    add glmmTMB_spatial_1

--- R/data.R
+++ R/data.R
@@ -545,6 +545,28 @@
 
 
 
+#' glmmTMB_spatial_1
+#'
+#' Model of class glmmTMB
+#' @examples
+#' \dontrun{
+#' library(geoR)
+#' data(ca20)
+#' dat <- data.frame(
+#'   x = ca20$coords[, 1],
+#'   y = ca20$coords[, 2],
+#'   calcium = ca20$data,
+#'   elevation = ca20$covariate[, 1],
+#'   region = factor(ca20$covariate[, 2])
+#' )
+#' dat$pos <- numFactor(scale(dat$x), scale(dat$y))
+#' dat$ID <- factor(rep(1, nrow(dat)))
+#'
+#' glmmTMB(calcium ~ elevation + region + mat(pos + 0 | ID), data = dat)
+#' }
+"glmmTMB_spatial_1"
+
+
 
 #' glmmTMB_1
 #'
