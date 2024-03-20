0e7ef8cc4c9b4c96a196f3680e25a944e91b1331
Daniel <mail@danielluedecke.de>
2024-03-20 10:39:09 +0100

    add model

--- R/data.R
+++ R/data.R
@@ -1659,6 +1659,28 @@
 "brms_mm_2"
 
 
+#' brms_mm_3
+#'
+#' Model of class brmsfit
+#' @examples
+#' \dontrun{
+#' set.seed(123)
+#' data(mzcars)
+#' mtcars$w <- 1
+#' brm(
+#'   mpg ~ 1 + hp + (1 | cyl) +
+#'     (1 | carb:am:hp) +
+#'     (1 | mm(carb, am, weights = cbind(w, w), scale = FALSE)) +
+#'     (1 | mm(carb:cyl, am:cyl, weights = cbind(w, w), scale = FALSE)),
+#'   data = mtcars, family = gaussian, chains = 4,
+#'   iter = 100, control = list(adapt_delta = 0.99, max_treedepth = 15),
+#'   cores = 4, seed = 12345,
+#'   backend = "cmdstanr"
+#' )
+#' }
+"brms_mm_3"
+
+
 #' brms_bf_1
 #'
 #' Model of class brmsfit
