96ac05fbaaf88d2b6b7b6dfaac0604679e2153ed
Daniel <mail@danielluedecke.de>
2024-05-29 16:34:16 +0200

    add brms sample model with monotonic effects

--- R/data.R
+++ R/data.R
@@ -1373,6 +1373,30 @@
 
 
 
+#' brms_mo1
+#'
+#' Model of class brmsfit
+#' @examples
+#' \dontrun{
+#' income_options <- c("below_20", "20_to_40", "40_to_100", "greater_100")
+#' income <- factor(
+#'   sample(income_options, 100, TRUE),
+#'   levels = income_options, ordered = TRUE
+#' )
+#' mean_ls <- c(30, 60, 70, 75)
+#' ls <- mean_ls[income] + rnorm(100, sd = 7)
+#' dat <- data.frame(income, ls) # fit a simple monotonic model
+#' brms_mo1 <- brm(ls ~ mo(income), data = dat)
+#' }
+"brms_mo1"
+
+
+
+
+
+
+
+
 #' brms_meta_1
 #'
 #' Model of class brmsfit
