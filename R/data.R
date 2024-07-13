01e39a677947901370c16b264226892f6d82fdf3
Daniel <mail@danielluedecke.de>
2024-07-13 21:12:58 +0200

    add model

--- R/data.R
+++ R/data.R
@@ -1513,6 +1513,31 @@
 
 
 
+#' brms_miss_1
+#'
+#' @examples
+#' \dontrun{
+#' dat <- read.csv("https://vincentarelbundock.github.io/Rdatasets/csv/carData/TitanicSurvival.csv")
+#' dat$survived <- ifelse(dat$survived == "yes", 1, 0)
+#' dat$woman <- ifelse(dat$sex == "female", 1, 0) |> as.factor()
+#' sum(is.na(dat$age))  # <-- 263 missing values
+#'
+#' brm(
+#'   bf(
+#'     survived ~ woman * mi(age) + passengerClass,
+#'     family = bernoulli(link = "logit")
+#'   ) +
+#'     bf(age | mi() ~ passengerClass + woman) +
+#'     set_rescor(FALSE),
+#'   data = dat,
+#'   backend = "cmdstanr",
+#'   cores = 4
+#' )
+#' }
+"brms_miss_1"
+
+
+
 
 #' brms_beta_1
 #'
