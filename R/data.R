90a8cf93bb0b540a1e8b61541e2541dc4a269277
Daniel <mail@danielluedecke.de>
2025-01-02 18:05:03 +0100

    add model

--- R/data.R
+++ R/data.R
@@ -1431,6 +1431,35 @@
 
 
 
+#' brms_sigma_2
+#'
+#' Model of class brmsfit
+#' @examples
+#' \dontrun{
+#' set.seed(123)
+#' d <- data.frame(
+#'   Condition = rep(c("A", "B", "C"), each=30),
+#'   Participant = as.factor(rep_len(paste0("S", 1:5), 90)),
+#'   Response = rnorm(90)
+#' )
+#' brm(
+#'   bf(
+#'     Response ~ Condition + (Condition | Participant),
+#'     sigma ~ Condition + (Condition | Participant),
+#'     family = exgaussian()
+#'   ),
+#'   data = d,
+#'   iter = 2000,
+#'   chains = 4,
+#'   cores = 4,
+#'   backend = "cmdstanr"
+#' )
+#' }
+"brms_sigma_2"
+
+
+
+
 #' brms_mo1
 #'
 #' Model of class brmsfit
