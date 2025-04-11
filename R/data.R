88f580eeda7009f8a66d08843935874290e29763
Daniel <mail@danielluedecke.de>
2025-04-11 11:39:23 +0200

    add model

--- R/data.R
+++ R/data.R
@@ -1312,6 +1312,32 @@
 "brms_logistic_1"
 
 
+
+#' brms_zoib_1
+#'
+#' Model of class brmsfit
+#' @examplesIf FALSE
+#' \dontrun{
+#' df <- data.frame(
+#'   y = cogmod::rbext(1000),
+#'   participant = rep(paste0("S", 1:10), each = 100)
+#' )
+#'
+#' f <- bf(
+#'   y ~ 1 + (1 | participant),
+#'   phi ~ 1 + (1 | participant),
+#'   zoi ~ 1 + (1 | participant),
+#'   coi ~ 1 + (1 | participant)
+#' )
+#'
+#' brm(f, data = df, family = zero_one_inflated_beta(), init = 0,
+#'     chains = 4, iter = 500, backend = "cmdstanr", refresh = 0)
+#' }
+"brms_zoib_1"
+
+
+
+
 #' brms_chocomini_1
 #'
 #' Model of class brmsfit
