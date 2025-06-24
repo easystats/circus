fd7ca2f0ceb2e1ceb5d9bc41835afc35243bfea8
Daniel <mail@danielluedecke.de>
2025-06-24 13:49:28 +0200

    docs

--- R/data.R
+++ R/data.R
@@ -1537,6 +1537,29 @@
 
 
 
+#' brms_mixture_1
+#'
+#' Model of class brmsfit
+#' @examplesIf FALSE
+#' \dontrun{
+#' brm(
+#'   formula = QoL ~ time + hospital + education + age + (1 + time | ID),
+#'   data = qol_cancer, # Your prepared dataset
+#'   family = mixture(gaussian, nmix = 3), # Gaussian distribution for outcome, K_classes components
+#'   chains = 4, # Number of MCMC chains
+#'   iter = 1000, # Total iterations per chain (includes warmup)
+#'   cores = 4,  # Number of CPU cores to use
+#'   seed = 1234 # For reproducibility
+#' )
+#' }
+"brms_mixture_1"
+
+
+
+
+
+
+
 #' brms_sigma_1
 #'
 #' Model of class brmsfit
